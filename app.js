d9a94260bb15b00aa8df8d1605f614ac48856240
asisow <38430905+asisow@users.noreply.github.com>
2019-08-08 16:48:31 +0300

    Post and fetch posts
    database name on localhost mongoDB:
    mongodb://localhost:27017/REST

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
-const path = require('path')
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-
-const feedRoutes = require('./routes/feed');
+const mongoose = require('mongoose')
+const path = require('path')
 
 const MONGODB_URI = 'mongodb://localhost:27017/REST';
 
+const feedRoutes = require('./routes/feed');
+
 const app = express();
 
-app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded()); // x-www-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
-})
+});
 
 app.use('/feed', feedRoutes);
 
@@ -26,16 +26,14 @@ app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message;
-    res.status(status).json({ message: messsage });
+    res.status(status).json({message: message});
 })
 
-mongoose.connect(MONGODB_URI)
-.then(() => {
-    console.log('database connected...');
-    app.listen(8080, () => {
-        console.log('Goliath online');
-    });
-})
-.catch(err => {
-    console.log(err);
-});
+mongoose
+    .connect(MONGODB_URI, { useNewUrlParser: true })
+    .then(() => {
+        app.listen(8080, () => console.log('Goliath online'));
+    })
+    .catch(err => {
+        console.log(err);    
+    })
--- routes/feed.js
+++ routes/feed.js
@@ -1,30 +1,24 @@
 const express = require('express');
-const {
-    body
-} = require('express-validator/check')
+const { body } = require('express-validator/check')
 
 const feedController = require('../controllers/feed');
 
 const router = express.Router();
 
-//GET /feed/posts
+// GET /feed/posts
 router.get('/posts', feedController.getPosts);
 
-//POST /feed/post
+// POST /feed/post
 router.post('/post', [
     body('title')
-        .trim()
-        .isLength({
-            min: 5
-        }),
+    .trim()
+    .isLength({ min: 5 }),
     body('content')
-        .trim()
-        .isLength({
-            min: 5
-        })
-], feedController.createPost);
+    .trim()
+    .isLength({ min: 5 }),
+], 
+feedController.createPost);
 
 router.get('/post/:postId', feedController.getPost);
-
 
 module.exports = router;
